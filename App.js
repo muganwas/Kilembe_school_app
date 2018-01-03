920d9da8a3fcf82b45734eae0f954a49b1e08712
Muganwa <stevenmuganwa@live.com>
2018-01-03 11:06:56 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { emailregex } from './otherJs/helpers';
+var kati = [];
+var lastEl = [];
+
+export default class App extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            username: null,
+            password: null,
+            hpassval: null,
+            passhidden: true,
+            feedback: null
+        }
+    }
+    login = ()=>{
+        
+        let uname = this.state.username;
+        let pass = this.state.password;
+        if( (uname !== null && uname !== null) && (pass !== null && pass !== undefined) ){
+            if(uname.match(emailregex)){
+                this.setState({
+                    feedback:null
+                })
+                let unameCount = (uname.split('')).length;
+                let passCount = (pass.split('')).length;
+                let unameFb = "*Username shouldn't be less than 5 characters.";
+                let passFb = "*Password should'nt be less than 5 characters.";
+                if(unameCount < 5){
+                    let feedback = unameFb; 
+                    if(passCount < 5){
+                        feedback = feedback + "\n" + passFb;
+                        this.setState({
+                            feedback: feedback
+                        });
+                    }else{
+                        this.setState({
+                            feedback: feedback
+                        });
+                    }
+                }else if(passCount < 5){
+                    feedback = passFb;
+                    this.setState({
+                        feedback: feedback
+                    });
+                }else{
+                    this.setState({
+                        feedback: null
+                    })
+                    console.log(uname + " " + pass);
+                }
+            }else{
+                this.setState({
+                    feedback: "The email address format is incorrect."
+                });
+            }   
+        }else{
+            this.setState({
+                feedback: "Both username and password are required."
+            });
+        }    
+    }
+    username = (event)=>{
+        this.setState({
+            username: event
+        })
+    }
+    password = (data)=>{
+        let event = data;
+        let eventArr = event.split('');
+        lastEl = eventArr.pop();
+        if(lastEl !== '*'){
+            kati.push(lastEl);
+        }else if(lastEl === '*'){
+            kati.pop();
+        }
+        let hiddenPass = data==undefined?"": data.replace(/./gi, '*');  
+        this.setState({
+            password: (kati.join('')),
+            hpassval: hiddenPass            
+        });
+    }
+    render() {
+        return ( 
+            <View style = { styles.container }>
+                <View style = { styles.login }>
+                    <Text style ={ styles.login_header }>Login</Text>
+                    <Text style ={ styles.feedback }>{ this.state.feedback }</Text>
+                    <View style = { styles.form }>
+                        <TextInput style = { styles.textField } onChangeText={ this.username} placeholder="Email Address" id="username" />
+                        <TextInput style = { styles.textField } value={ this.state.hpassval } onChangeText={ this.password } placeholder="Password" id="passord" />
+                        <Button onPress={ this.login } title="submit" value="Submit" />
+                    </View>
+                </View>  
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    login: {
+        width: '70%'
+    },
+    login_header: {
+        fontSize: 20,
+        marginBottom: 10
+    },
+    textField: {
+        padding: 5
+    },
+    form: {
+        borderWidth: 0.3,
+        borderColor: '#d9d9d9',
+        padding: 10
+    },
+    feedback: {
+        padding: 5,
+        fontSize: 16,
+        color: '#8e8d8a'
+    }
+});
