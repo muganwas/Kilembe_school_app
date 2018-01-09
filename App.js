810898b7932b4734efc2526a61a977f91d45a852
Muganwa <stevenmuganwa@live.com>
2018-01-10 01:04:50 +0300

    boxed with facebook_login, wasn't easy

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import { emailregex } from './otherJs/helpers';
-import { passRegex } from './otherJs/helpers';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import { emailregex, passRegex } from './otherJs/helpers';
+//import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
+import FBSDK, { LoginManager } from 'react-native-fbsdk';
 var kati = [];
 export default class App extends React.Component {
     constructor(){
@@ -69,6 +70,37 @@ export default class App extends React.Component {
             });
         }    
     }
+    /*_googleSignin = ()=>{
+        GoogleSignin.hasPlayServices({ autoResolve: true }).then(() => {
+            // play services are available. can now configure library
+            //alert('all good');
+            GoogleSignin.configure({
+                scopes: [ 'https://www.googleapis.com/auth/userinfo.email' ],
+                shouldFetchBasicProfile: true,
+                webClientId: "365926543341-rjit9be1m995msb01jq822tfnhcfkebp.apps.googleusercontent.com",//from developers console
+                //forceConsentPrompt: true, // [Android] if you want to show the authorization prompt at each login
+              }).then(() => {
+                alert('fuck u everyone');
+              });
+        }).catch((err) => {
+          console.log("Play services error", err.code, err.message);
+        })
+    }*/
+    _fbSignin = ()=> {
+        LoginManager.logInWithReadPermissions(['public_profile']).then(
+            function(result) {
+              if (result.isCancelled) {
+                alert('Login cancelled');
+              } else {
+                alert('Login success with permissions: '
+                  +result.grantedPermissions.toString());
+              }
+            },
+            function(error) {
+              alert('Login fail with error: ' + error);
+            }
+        );
+    }
     username = (event)=>{
         this.setState({
             username: event
@@ -109,7 +141,10 @@ export default class App extends React.Component {
                     <View style = { styles.form }>
                         <TextInput style = { styles.textField } onChangeText={ this.username} placeholder="Email Address" id="username" />
                         <TextInput style = { styles.textField } value={ this.state.hpassval } onChangeText={ this.password } placeholder="Password" id="passord" />
-                        <Button onPress={ this.login } title="submit" value="Submit" />
+                        <Button onPress={ this.login } title="Sign in"/>
+                        <Text style={ styles.ran_info }>Or</Text>
+                        <TouchableOpacity onPress={ this._fbSignin }><Text>Login with Facebook</Text>
+                        </TouchableOpacity>
                     </View>
                 </View>  
             </View>
@@ -123,6 +158,11 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    ran_info: {
+        textAlign: 'center',
+        fontSize: 17,
+        padding: 3
     },
     login: {
         width: '70%'
@@ -143,5 +183,9 @@ const styles = StyleSheet.create({
         padding: 5,
         fontSize: 16,
         color: '#8e8d8a'
+    },
+    googleButton: {
+        width: '100%',
+        height: 48
     }
 });
